fda1937f9dda69debbb1c9ebeab24a154918c79a
Bernardo F. Fraga <bfoquesdefraga@gmail.com>
2025-06-16 23:02:02 +0000

    updated FPU.sv and tb_sv

--- HDL/FPU.sv
+++ HDL/FPU.sv
@@ -10,17 +10,17 @@ output logic [3:0] status_out,
 
 );
 
-reg [19:0] mant_a, mant_b, mant_r, mant_s; // 20bits (19:0)
-reg [10:0] exp_a, exp_b, exp_r, dif;   // 11 bits (30:20)
-reg sign_a, sign_b, sign_r;        // 1 bit (31)
-reg ov_flag, und_flag, inex_flag, exct_flag; // flags
+logic [19:0] mant_a, mant_b, mant_r, mant_s; // 20bits (19:0)
+logic [10:0] exp_a, exp_b, exp_r, dif;   // 11 bits (30:20)
+logic [2:0]  rounding_bits;
+logic sign_a, sign_b, sign_r;        // 1 bit (31)
 
-typedef enum {DIVIDE, PRE_ADD, ADD, NORMALIZER, SO, OVERFLOW, UNDERFLOW, INEXECT, EXACT} statetype;
+typedef enum {DIVIDE, PRE_ADD, ADD, NORMALIZER, ROUNDING, OUTPUT_RESULT} statetype;
 statetype states;
 
 
-always @ (posedge clock_100k) begin 
-    if (reset) begin
+always @ (posedge clock_100k or negedge reset) begin 
+    if (!reset) begin
 
         op_a       <= 32'd0;
         op_b       <= 32'd0;
@@ -28,24 +28,19 @@ always @ (posedge clock_100k) begin
         status_out <= 4'd0;
 
         mant_a     <= 20'd0;
-        exp_a      <= 11'd0;
+        exp_a      <= 10'd0;
         sign_a     <= 1'd0;
   
   
         mant_b     <= 20'd0;
-        exp_b      <= 11'd0;
+        exp_b      <= 10'd0;
         sign_b     <= 1'd0;
             
         mant_r     <= 20'd0;
-        exp_r      <= 11'd0;
+        exp_r      <= 10'd0;
         sign_r     <= 1'd0;
     
-        dif        <= 11'd0;
-        ov_flag    
-        und_flag
-        inex_flag
-        exct_flag
-        
+        dif        <= 10'd0;
 
         states <= DIVIDE;
     end else begin 
@@ -69,91 +64,81 @@ always @ (posedge clock_100k) begin
             //se os expoentes forem iguais, então vai direto para soma
             if(exp_a == exp_b) begin
                 exp_r <= exp_a;
-                states <= ADD;
 
-            end else begin
-                //se o expoente a for maior que b, então faz a diferença dos expoentes e usa esse expoente na soma e shifta a menor mantissa(b) 
-                if (exp_a > exp_b) begin
+            end else if (exp_a > exp_b) begin        //se o expoente a for maior que b, então faz a diferença dos expoentes e usa esse expoente na soma e shifta a menor mantissa(b) 
                     dif <= (exp_a - exp_b); 
                     mant_b <= mant_b >> (exp_a - exp_b); 
                     exp_b = exp_a;
                     exp_r <= exp_b;
-                    states <= ADD;
-
-                end else begin
+                    
+            end else begin
                     //se o expoente b for maior que a, então faz a diferença dos expoentes e usa esse expoente na soma e shifta a menor mantissa(a)
-                    if (exp_b > exp_a) begin
-                        dif <= (exp_b - exp_a);
-                        mant_a <= mant_a >> (exp_b - exp_a);
-                        exp_a = exp_b;
-                        exp_r <= exp_a;
-                        states <= ADD;
-
+                    dif <= (exp_b - exp_a);
+                    mant_a <= mant_a >> (exp_b - exp_a);
+                    exp_a = exp_b;
+                    exp_r <= exp_a;
                     end
+                states <= ADD;
                 end
-            end
-        end 
 
         ADD: begin
         if (sign_a == sign_b) begin
             mant_r <= mant_a + mant_b;
-            sign_r <= sign_a;
-            states <= SO;      
+            sign_r <= sign_a;   
 
-        end else begin
-            if (sign_a > sign_b) begin
+        end else if (sign_a > sign_b) begin
             mant_r <= mant_a - mant_b;
             sign_r <= sign_a;
-            states <= NORMALIZER;
-            end
 
         end else begin
-            if(sign_b > sign_a) begin
             mant_r <= mantb - mant_a;
             sign_r <= sign_b;
-            states <= NORMALIZER;
-            end
         end
+            states <= NORMALIZER;
 
     end
 
         NORMALIZER: begin
             if(mant_r[20] == 1'b1 ) begin
-                mant_r <= mant_r >> dif;
-                exp_r  <= exp_r + dif;
-                states <= SO;
+               mant_r <= mant_r >> 1;
+               exp_r  <= exp_r + 1;
+               rounding_bits <= {mant_r[0], 1'b0, 1'b0};
                 
             end else begin
-                while (mant_r[23] != 1'b1 && exp_r > 0) begin
+                while (mant_r[19] != 1'b1 && exp_r > 0) begin
                 mant_r = mant_r << 1;
-                 exp_r = exp_r - 1;
-                 states <= SO;
+                exp_r = exp_r - 1;
+                end
+                rounding_bits <= 3'b000;
             end
+            state <= ROUNDING;
         end
         
-        SO: begin
-            if (exp_r > 8'd254) begin
-            status_out[2] = 1'b1;  // OVERFLOW
+       ROUNDING: begin
+                    // Checagem de bits de arredondamento para INEXACT
+                    if (rounding_bits != 3'b000)
+                        inex_flag <= 1;
+                    else
+                        exct_flag <= 1;
 
-            end else begin
-                if (exp_r < 8'd1 && mant_r != 0) begin
-                status_out[1] = 1'b1;  // UNDERFLOW
+                    // Checar overflow e underflow
+                    if (exp_r > 11'd2046) // limite para 11 bits
+                        ov_flag <= 1;
+                    else if (exp_r == 0 && mant_r != 0)
+                        und_flag <= 1;
+                    state <= OUTPUT_RESULT;
                 end
 
-            end else begin
-                    if (guard_bit == 1'b1 || round_bit == 1'b1 || sticky_bit == 1'b1) begin
-                    status_out[0] = 1'b1;  // INEXACT
-                    end
-                    
-            end else begin    
-                    if (status_out == 4'b0000) begin
-                    status_out[3] = 1'b1;  // EXACT
-                    end
-            end
-
+                OUTPUT_RESULT: begin
+                    data_out <= {sign_r, exp_r, mant_r[18:0]};
+                    status_out <= 4'b0000;
+                    if (exct_flag) status_out[3] <= 1'b1;
+                    if (ov_flag)   status_out[2] <= 1'b1;
+                    if (und_flag)  status_out[1] <= 1'b1;
+                    if (inex_flag) status_out[0] <= 1'b1;
+                    state <= DIVIDE; // reinicia para próxima operação
+                end
+            endcase
         end
-
-
     end
-end
 endmodule
--- TB/tb.sv
+++ TB/tb.sv
@@ -9,25 +9,79 @@ logic [31:0] data_out;
 logic [3:0] status_out;
 
     FPU dut ( 
-    .clock_100k(clock_100k),
-    .reset(reset),
+    .clock_100k(clk),
+    .reset(rst),
     .op_a(op_a),
     .op_b(op_b),
     .data_out(data_out),
     .status_out(status_out)
     );
     
-always #5 clock_100k = ~clock_100k;
+always #5 clk = ~clk;
 
     initial begin
-        reset = 1;
+        $display("Iniício dos testes");
+        rst = 0;
         op_a = 32'b0;
         op_b = 32'b0;
         #20;
-        reset = 0;
+        
         #100;
-        reset = 1;
+        rst = 1;
         #50;
-        // Formato: {sinal, expoente[11], mantissa[20]}
+
+        //  Teste 1: Soma simples 1.0 + 1.0
+        test(32'b0_10000000001_00000000000000000000,    //1.0
+             32'b0_10000000001_00000000000000000000);   //1.0
+
+        //  Teste 2: Subtração simples 2.0 - 1.0
+        test(32'b0_10000000010_00000000000000000000,    //2.0
+             32'b1_10000000001_00000000000000000000);   //-1.0
+
+        //  Teste 3: Soma de 0 + 3.5
+        test(32'b0_00000000000_00000000000000000000,    //2.0
+             0_10000000001_10000000000000000000);   //-1.0
+
+        //  Teste 4: Subtração do mesmo número
+        test(32'b0_10000000001_00000000000000000000,    //1.0
+             32'b1_10000000001_00000000000000000000);   //-1.0
+
+        //  Teste 5: Overflow  (expoente muito alto)
+        test(32'b0_11111111111_00000000000000000000,    // Expoente Máximo
+             32'b0_11111111111_00000000000000000000);   
+
+        //  Teste 6: Underflow (mantissa pequena com expoente mínimo)
+        test(32'b0_00000000001_00000000000000000001,    
+             32'b0_00000000001_00000000000000000001);  
+
+        //  Teste 7: Inexact
+        test(32'b0_10000000001_00000000000000000001,    //1.000...01
+             32'b0_10000000001_00000000000000000001);   //1.000...01
+
+        //  Teste 8: Soma Negativa -2.0 + -1.0
+        test(32'b1_10000000010_00000000000000000000,    //-2.0
+             32'b1_10000000001_00000000000000000000);   //-1.0
+
+        //  Teste 9: Soma 0 + 0
+        test(32'b0_00000000000_00000000000000000000,    
+             32'b0_00000000000_00000000000000000000);   
+
+        //  Teste 10: Subtração invertida 1.0 - 2.0
+        test(32'b0_10000000001_00000000000000000000,    //1.0
+             32'b1_10000000010_00000000000000000000);   //-2.0
+    
+    end
+
+task test(input logic [31:0] a, input logic [31:0] b);
+    begin 
+    @(negedge clk);
+    op_a <= a;
+    op_b <= b;
+
+    @(posedge clk); repeat (8) @(posedge clk); // Aguarda os ciclos
+    $display("OpA = %b, OpB = %b -> Result = %b, Status = %b", a, b, data_out, status_out);
+    end
+endtask
+
 
 endmodule
