92157020fcc9d0351855c3ea5f38c758a56fa2f4
Bernardo F. Fraga <bfoquesdefraga@gmail.com>
2025-06-18 16:34:54 +0000

    finished all project

--- HDL/FPU.sv
+++ HDL/FPU.sv
@@ -1,144 +1,197 @@
 module FPU (
-
-input logic clock_100k,
-input logic reset,
-input logic [31:0] op_a,
-input logic [31:0] op_b,
-
-output logic [31:0] data_out,
-output logic [3:0] status_out,
-
+    input  logic        clock_100k,
+    input  logic        reset,       // reset assíncrono ativo em nível baixo
+    input  logic [31:0] op_a,
+    input  logic [31:0] op_b,
+    output logic [31:0] data_out,
+    output logic [3:0]  status_out  // [3] EXACT, [2] OVERFLOW, [1] UNDERFLOW, [0] INEXACT
 );
 
-logic [19:0] mant_a, mant_b, mant_r, mant_s; // 20bits (19:0)
-logic [10:0] exp_a, exp_b, exp_r, dif;   // 11 bits (30:20)
-logic [2:0]  rounding_bits;
-logic sign_a, sign_b, sign_r;        // 1 bit (31)
+    // Campos dos operandos
+    logic        sign_a, sign_b, sign_r;
+    logic [10:0] exp_a, exp_b, exp_r;
+    logic [20:0] mant_a, mant_b, mant_r;
 
-typedef enum {DIVIDE, PRE_ADD, ADD, NORMALIZER, ROUNDING, OUTPUT_RESULT} statetype;
-statetype states;
+    // Variáveis pipeline
+    logic [20:0] mant_add;
+    logic        sign_add;
+    logic [10:0] exp_next;
 
+    // Status
+    logic ov_flag, und_flag, inex_flag, exct_flag;
 
-always @ (posedge clock_100k or negedge reset) begin 
-    if (!reset) begin
+    typedef enum logic [3:0] {
+        DIVIDE, PRE_ADD, WAIT_PRE_ADD,
+        ADD, WAIT_ADD,
+        NORMALIZER, ROUNDING, OUTPUT_RESULT
+    } statetype;
+    statetype states;
 
-        op_a       <= 32'd0;
-        op_b       <= 32'd0;
-        data_out   <= 32'd0;
-        status_out <= 4'd0;
+    always_ff @(posedge clock_100k or negedge reset) begin
+        if (!reset) begin
+            data_out   <= 32'd0;
+            status_out <= 4'd0;
+            mant_a     <= 21'd0;
+            exp_a      <= 11'd0;
+            sign_a     <= 1'b0;
 
-        mant_a     <= 20'd0;
-        exp_a      <= 10'd0;
-        sign_a     <= 1'd0;
-  
-  
-        mant_b     <= 20'd0;
-        exp_b      <= 10'd0;
-        sign_b     <= 1'd0;
-            
-        mant_r     <= 20'd0;
-        exp_r      <= 10'd0;
-        sign_r     <= 1'd0;
-    
-        dif        <= 10'd0;
+            mant_b     <= 21'd0;
+            exp_b      <= 11'd0;
+            sign_b     <= 1'b0;
 
-        states <= DIVIDE;
-    end else begin 
-        case (states)
+            mant_r     <= 21'd0;
+            exp_r      <= 11'd0;
+            sign_r     <= 1'b0;
 
-        DIVIDE: begin
-            sign_a <= op_a[31];
-            sign_b <= op_b[31];
+            mant_add   <= 21'd0;
+            sign_add   <= 1'b0;
+            exp_next   <= 11'd0;
 
-            exp_a <= op_a [30:20];
-            exp_b <= op_b [30:20];
-            
+            ov_flag    <= 1'b0;
+            und_flag   <= 1'b0;
+            inex_flag  <= 1'b0;
+            exct_flag  <= 1'b0;
 
-            mant_a <= op_a [19:0];
-            mant_b <= op_b [19:0];
-
-            states <= PRE_ADD;
-        end
-
-        PRE_ADD: begin
-            //se os expoentes forem iguais, então vai direto para soma
-            if(exp_a == exp_b) begin
-                exp_r <= exp_a;
-
-            end else if (exp_a > exp_b) begin        //se o expoente a for maior que b, então faz a diferença dos expoentes e usa esse expoente na soma e shifta a menor mantissa(b) 
-                    dif <= (exp_a - exp_b); 
-                    mant_b <= mant_b >> (exp_a - exp_b); 
-                    exp_b = exp_a;
-                    exp_r <= exp_b;
-                    
-            end else begin
-                    //se o expoente b for maior que a, então faz a diferença dos expoentes e usa esse expoente na soma e shifta a menor mantissa(a)
-                    dif <= (exp_b - exp_a);
-                    mant_a <= mant_a >> (exp_b - exp_a);
-                    exp_a = exp_b;
-                    exp_r <= exp_a;
-                    end
-                states <= ADD;
-                end
-
-        ADD: begin
-        if (sign_a == sign_b) begin
-            mant_r <= mant_a + mant_b;
-            sign_r <= sign_a;   
-
-        end else if (sign_a > sign_b) begin
-            mant_r <= mant_a - mant_b;
-            sign_r <= sign_a;
-
+            states <= DIVIDE;
         end else begin
-            mant_r <= mantb - mant_a;
-            sign_r <= sign_b;
-        end
-            states <= NORMALIZER;
-
-    end
-
-        NORMALIZER: begin
-            if(mant_r[20] == 1'b1 ) begin
-               mant_r <= mant_r >> 1;
-               exp_r  <= exp_r + 1;
-               rounding_bits <= {mant_r[0], 1'b0, 1'b0};
-                
-            end else begin
-                while (mant_r[19] != 1'b1 && exp_r > 0) begin
-                mant_r = mant_r << 1;
-                exp_r = exp_r - 1;
+            case (states)
+                DIVIDE: begin
+                    $display("entrei no divide");
+                    $display("---------------------------------------------------");
+                    sign_a <= op_a[31];
+                    sign_b <= op_b[31];
+                    exp_a  <= op_a[30:20];
+                    exp_b  <= op_b[30:20];
+                    mant_a <= (op_a[30:20] != 0) ? {1'b1, op_a[19:0]} : {1'b0, op_a[19:0]};
+                    mant_b <= (op_b[30:20] != 0) ? {1'b1, op_b[19:0]} : {1'b0, op_b[19:0]};
+                    ov_flag    <= 1'b0;
+                    und_flag   <= 1'b0;
+                    inex_flag  <= 1'b0;
+                    exct_flag  <= 1'b0;
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                   $display("---------------------------------------------------");
+                    states <= PRE_ADD;
                 end
-                rounding_bits <= 3'b000;
-            end
-            state <= ROUNDING;
-        end
-        
-       ROUNDING: begin
-                    // Checagem de bits de arredondamento para INEXACT
-                    if (rounding_bits != 3'b000)
-                        inex_flag <= 1;
-                    else
-                        exct_flag <= 1;
 
-                    // Checar overflow e underflow
-                    if (exp_r > 11'd2046) // limite para 11 bits
-                        ov_flag <= 1;
-                    else if (exp_r == 0 && mant_r != 0)
-                        und_flag <= 1;
-                    state <= OUTPUT_RESULT;
+                PRE_ADD: begin
+                    $display("entrei no pre_add");
+                    $display("---------------------------------------------------");
+                    if (exp_a == exp_b) begin
+                        exp_next <= exp_a;
+                    end else if (exp_a > exp_b) begin
+                        mant_b <= mant_b >> (exp_a - exp_b);
+                        exp_next <= exp_a;
+                    end else begin
+                        mant_a <= mant_a >> (exp_b - exp_a);
+                        exp_next <= exp_b;
+                    end
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                   $display("---------------------------------------------------");
+                    states <= WAIT_PRE_ADD;
+                end
+
+                WAIT_PRE_ADD: begin
+                    $display("entrei no wait_pre_add");
+                    $display("---------------------------------------------------");
+                    exp_r <= exp_next;
+                     $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                   $display("---------------------------------------------------");
+                    states <= ADD;
+                end
+
+                ADD: begin
+                    $display("entrei no add");
+                    $display("---------------------------------------------------");
+                    if (sign_a == sign_b) begin
+                        mant_add <= (mant_a + mant_b);
+                        sign_add <= sign_a;
+                    end else if (mant_a >= mant_b) begin
+                        mant_add <= (mant_a - mant_b);
+                        sign_add <= sign_a;
+                    end else begin
+                        mant_add <= (mant_b - mant_a);
+                        sign_add <= sign_b;
+                    end
+                     $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                   $display("---------------------------------------------------");
+                    states <= WAIT_ADD;
+                end
+
+                WAIT_ADD: begin
+                    $display("entrei no wait_add");
+                    $display("---------------------------------------------------");
+                    mant_r <= mant_add;
+                    sign_r <= sign_add;
+                     $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                   $display("---------------------------------------------------");
+                    states <= NORMALIZER;
+                end
+
+                NORMALIZER: begin
+                    $display("entrei no normalizer");
+                    $display("---------------------------------------------------");
+                    if (mant_r[21]) begin
+                        inex_flag <= inex_flag | mant_r[0];
+                        mant_r <= mant_r >> 1;
+                        exp_r  <= exp_r + 1;
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                    $display("---------------------------------------------------");
+                        states <= ROUNDING;
+                       
+                    end else if (!mant_r[20] && exp_r > 0 && mant_r != 0) begin
+                        mant_r <= mant_r << 1;
+                        exp_r  <= exp_r - 1;
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                    $display("---------------------------------------------------");
+                        states <= ROUNDING;
+                    end else begin
+                        ov_flag   <= (exp_r > 11'd2046);
+                        und_flag  <= (exp_r == 0 && mant_r[19:0] != 0);
+                        exct_flag <= ~inex_flag;
+                        $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                        $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                        $display("---------------------------------------------------");
+                        states <= ROUNDING;
+                    end
+                end
+                ROUNDING: begin
+                    $display("entrei no rounding");
+                    $display("---------------------------------------------------");
+                    if (ov_flag) begin
+                        exp_r  <= 11'd2046;
+                        mant_r <= 21'd0;
+                    end
+                    status_out <= {exct_flag, ov_flag, und_flag, inex_flag};
+                     $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                     $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                     $display("---------------------------------------------------");
+                    states <= OUTPUT_RESULT;
                 end
 
                 OUTPUT_RESULT: begin
-                    data_out <= {sign_r, exp_r, mant_r[18:0]};
-                    status_out <= 4'b0000;
-                    if (exct_flag) status_out[3] <= 1'b1;
-                    if (ov_flag)   status_out[2] <= 1'b1;
-                    if (und_flag)  status_out[1] <= 1'b1;
-                    if (inex_flag) status_out[0] <= 1'b1;
-                    state <= DIVIDE; // reinicia para próxima operação
+                    $display("entrei no output_result");
+                    $display("---------------------------------------------------");
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_a, exp_a, mant_a);
+                   $display("DEBUG: sign=%b exp=%h mant=%h", sign_b, exp_b, mant_b);
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_r, exp_r, mant_r);
+                    $display("---------------------------------------------------");
+                    data_out <= {sign_r, exp_r, mant_r[19:0]};
+                    $display("DEBUG: sign=%b exp=%h mant=%h", sign_r, exp_r, mant_r);
+                    $display("---------------------------------------------------");
+                    $display("terminei");
+                    $display("---------------------------------------------------");
+                    states <= DIVIDE;
                 end
+
             endcase
         end
     end
+
 endmodule
--- TB/tb.sv
+++ TB/tb.sv
@@ -1,87 +1,55 @@
 `timescale 1us/1ns
 module tb;
 
-logic clock_100k;
-logic reset;
-logic [31:0] op_a;
-logic [31:0] op_b;
-logic [31:0] data_out;
-logic [3:0] status_out;
+    logic clock_100k;
+    logic reset;
+    logic [31:0] op_a;
+    logic [31:0] op_b;
+    logic [31:0] data_out;
+    logic [3:0] status_out;
 
     FPU dut ( 
-    .clock_100k(clk),
-    .reset(rst),
-    .op_a(op_a),
-    .op_b(op_b),
-    .data_out(data_out),
-    .status_out(status_out)
+        .clock_100k(clock_100k),
+        .reset(reset),
+        .op_a(op_a),
+        .op_b(op_b),
+        .data_out(data_out),
+        .status_out(status_out)
     );
-    
-always #5 clk = ~clk;
+
+    always #5 clock_100k = ~clock_100k;
 
     initial begin
-        $display("Iniício dos testes");
-        rst = 0;
-        op_a = 32'b0;
-        op_b = 32'b0;
-        #20;
-        
-        #100;
-        rst = 1;
-        #50;
+        $display("-------------------------------");
+        $display("Inicio dos Testes\n");
+        clock_100k = 0;
+        reset = 0;
+        op_a = 0;
+        op_b = 0;
+        #10;
+        reset = 1;
+        #10;
+        //soma simples 1 + 1
+        op_a = 32'b00000000000000000000000000000001;
+        op_b = 32'b00000000000000000000000000000001;        
+        #170;
+        // subtração simples 2 - 1
+        op_a = 32'b00000000000000000000000000000010;
+        op_b = 32'b10000000000000000000000000000001; 
+        #170;
+        //com overflow
+        op_a = 32'b01111111111111111111111111111111;
+        op_b = 32'b01111111111111111111111111111111;
+        #300;
+        // soma com ponto flutuante
+        op_a = 32'b0_10000000000_01000000000000000000; // 2.5
+        op_b = 32'b0_10000000000_01000000000000000000; // 2.5
+        #300;
 
-        //  Teste 1: Soma simples 1.0 + 1.0
-        test(32'b0_10000000001_00000000000000000000,    //1.0
-             32'b0_10000000001_00000000000000000000);   //1.0
+        $display("\n Testes finalizados.");
+        $stop;
 
-        //  Teste 2: Subtração simples 2.0 - 1.0
-        test(32'b0_10000000010_00000000000000000000,    //2.0
-             32'b1_10000000001_00000000000000000000);   //-1.0
 
-        //  Teste 3: Soma de 0 + 3.5
-        test(32'b0_00000000000_00000000000000000000,    //2.0
-             0_10000000001_10000000000000000000);   //-1.0
-
-        //  Teste 4: Subtração do mesmo número
-        test(32'b0_10000000001_00000000000000000000,    //1.0
-             32'b1_10000000001_00000000000000000000);   //-1.0
-
-        //  Teste 5: Overflow  (expoente muito alto)
-        test(32'b0_11111111111_00000000000000000000,    // Expoente Máximo
-             32'b0_11111111111_00000000000000000000);   
-
-        //  Teste 6: Underflow (mantissa pequena com expoente mínimo)
-        test(32'b0_00000000001_00000000000000000001,    
-             32'b0_00000000001_00000000000000000001);  
-
-        //  Teste 7: Inexact
-        test(32'b0_10000000001_00000000000000000001,    //1.000...01
-             32'b0_10000000001_00000000000000000001);   //1.000...01
-
-        //  Teste 8: Soma Negativa -2.0 + -1.0
-        test(32'b1_10000000010_00000000000000000000,    //-2.0
-             32'b1_10000000001_00000000000000000000);   //-1.0
-
-        //  Teste 9: Soma 0 + 0
-        test(32'b0_00000000000_00000000000000000000,    
-             32'b0_00000000000_00000000000000000000);   
-
-        //  Teste 10: Subtração invertida 1.0 - 2.0
-        test(32'b0_10000000001_00000000000000000000,    //1.0
-             32'b1_10000000010_00000000000000000000);   //-2.0
-    
     end
-
-task test(input logic [31:0] a, input logic [31:0] b);
-    begin 
-    @(negedge clk);
-    op_a <= a;
-    op_b <= b;
-
-    @(posedge clk); repeat (8) @(posedge clk); // Aguarda os ciclos
-    $display("OpA = %b, OpB = %b -> Result = %b, Status = %b", a, b, data_out, status_out);
-    end
-endtask
-
 
 endmodule
